f11cc51a4f7f94c880fc5e76354e1c9fe7f982b4
cowhorse7 <rnc23@byu.edu>
2025-07-14 16:28:23 -0600

    list name edit working

--- src/app/pages/lists/lists.page.ts
+++ src/app/pages/lists/lists.page.ts
@@ -5,6 +5,9 @@ import { TRPCService } from '../../trpc.service';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 type List = Prisma.ListGetPayload<{
   include: {items: true};
@@ -12,7 +15,7 @@ type List = Prisma.ListGetPayload<{
 
 @Component({
   selector: 'app-lists',
-  imports: [MatCheckboxModule, FormsModule, CommonModule],
+  imports: [MatCheckboxModule, FormsModule, CommonModule, MatIconModule, MatFormField, MatInputModule],
   templateUrl: './lists.page.html',
   styleUrl: './lists.page.scss'
 })
@@ -21,6 +24,7 @@ export class ListsPage implements OnInit {
   currentUserId: string|undefined = undefined;
   lists: List[] = [];
   activeLists: Set<number> = new Set();
+  editingListId: number | null = null;
 
   constructor(private authService: AuthService) {}
 
@@ -30,6 +34,14 @@ export class ListsPage implements OnInit {
     if(this.currentUserId !== undefined){
       this.lists = await this.trpc.list.getLists.mutate();
     }
+  }
+
+  toggleEditing(listId: number){
+    this.editingListId = this.editingListId === listId ? null : listId;
+  }
+  
+  async editList(listId: number, newName: string){
+    await this.trpc.list.updateList.mutate({listId: listId, name: newName});
   }
 
   toggleList(listId: number): void {
@@ -48,4 +60,5 @@ export class ListsPage implements OnInit {
     item.completed = newCompletedValue;
     //add in trpc call to update item
   }
+
 }
